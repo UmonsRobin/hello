328ac9ea67cc85c3013d898ad52a19ab3a564a4e
Robin_C <robchoquet@hotmail.com>
2016-02-15 19:39:36 +0100

    Ajout de la méthode main à la classe square

--- comm/mons/modele/pawn.java
+++ comm/mons/modele/pawn.java
@@ -3,5 +3,10 @@ package comm.mons.modele;
 public class pawn {
 
 	private String[] tableau;
+	
+	public static void main(String[] args){
+		System.out.println("je test la branche square");
+	}
+
 			
 }
